2ed15ee649a6f9f3f9831c05c4f92d37a6c1d2f3
Daniel Husar <dano.husar@gmail.com>
2017-07-19 00:08:23 +0200

    Add http methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   constraints(host: /^(?!www\.)/i) do
     match "(*any)" => redirect { |params, request|
       URI.parse(request.url).tap { |uri| uri.host = "www.#{uri.host}" }.to_s
-    }
+    },  via: [:get, :post, :put, :delete]
   end
 
   root to: "site#homepage"
