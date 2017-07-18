cce44cc02ffae1464ccd3a5d3184f991e4a4f22b
Daniel Husar <dano.husar@gmail.com>
2017-07-19 00:03:17 +0200

    force www

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  constraints(host: /^(?!www\.)/i) do
+    match "(*any)" => redirect { |params, request|
+      URI.parse(request.url).tap { |uri| uri.host = "www.#{uri.host}" }.to_s
+    }
+  end
+
   root to: "site#homepage"
   # get "/:locale", to: "main#homepage"
 end
