f64240607d0a7d0a53fd0b034813efb6b9a95185
Daniel Husar <dano.husar@gmail.com>
2017-07-20 21:25:35 +0200

    Tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
     match "(*any)" => redirect { |params, request|
       URI.parse(request.url).tap { |uri| uri.host = "www.#{uri.host}" }.to_s
     },  via: [:get, :post, :put, :delete]
-  end
+  end if Rails.env.production?
 
   root to: "site#homepage"
-  # get "/:locale", to: "main#homepage"
+  get "/api/subscribe", to: "api#subscribe"
 end
